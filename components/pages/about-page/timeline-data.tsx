c257f27989d0b9fc0fa95b12e66dc1fe9d4722df
emeka <emekaignatius5@gmail.com>
2024-10-05 16:11:26 +0100

    chore: corrected timeline data

--- components/pages/about-page/timeline-data.tsx
+++ components/pages/about-page/timeline-data.tsx
@@ -117,14 +117,14 @@ community.`}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="/IMG_0143~3.JPG"
+              src="/IMG-20190619-WA0005.jpg"
               alt="Obikels Creations Ltd"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="/IMG-20190607-WA0033.jpg"
+              src="/IMG_0143~3.JPG"
               alt="Obikels Creations Ltd"
               width={500}
               height={500}
